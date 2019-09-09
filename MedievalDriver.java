6509a06dc2c459d2d3e6e07e8189c7cd07be003e
ScottJCraigII <54555670+ScottJCraigII@users.noreply.github.com>
2019-09-09 00:11:15 -0400

    Add files via upload

--- MedievalDriver.java
+++ MedievalDriver.java
@@ -1,5 +1,13 @@
-//Scott Craig
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author Scott
+ */
 public class MedievalDriver {
     public static void main(String[] args) {
 		Character king = new King("Thomas");
